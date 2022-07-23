26982dfa246ae30925bdbfffde51f92c4eb8c232
AngelSzr <angel.emilio@hotmail.com>
2022-07-22 19:30:21 -0500

    Aquí ya funciona

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,34 @@
 import Card from './Card'
-import Navbar from './Navbar'
 import '../App.css'
+import { IoMdPlanet } from 'react-icons/io'
+import { useRef, useState } from 'react'
+//hooks
+import useFetcher from '../hooks/useFetcher'
 
 
 function App() {
-  console.log(Navbar.info)
+  const [query, setQuery] = useState(`${new Date().getFullYear()}-${new Date().getUTCMonth()}-${new Date().getUTCDate()}`) //Aqui que agregue la fecha del día que se consulta
+  const { dataState: info, loading } = useFetcher(query)
+  const searchRef = useRef(null)
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setQuery(searchRef.current.value)
+  }
+
   return (
     <div>
-      <Navbar />
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand text-center fs-4"> <IoMdPlanet className='fs-1' /> NASA Astronomy Picture of the Day</a>
+          <form onSubmit={handleSubmit} className="d-flex">
+            <input className="form-control me-2" type="date" placeholder="Fecha" aria-label="Search" ref={searchRef} max={`${new Date().getFullYear()}-${new Date().getUTCMonth() + 1}-${new Date().getUTCDate()}`} />
+            <button className="btn btn-dark" type="submit" >Buscar</button>
+          </form>
+        </div>
+      </nav>
       <div className="container">
-        <Card />
+        {loading ? '<Loading />' : <Card data={info} />}
       </div>
-
     </div>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,16 @@
+
 const Card = ({ data }) => {
     return (
-        <div className="card text-center mt-5">
+        <div className="card text-center mt-3 w-auto">
             <div className="card-header">
-                <h5 className="card-title">Special title treatment</h5>
+                <h5 className="card-title">{data.title}</h5>
             </div>
             <div className="card-body">
-                <img src="..." className="img-fluid" alt="..." />
-                <p className="card-text">Fecha</p>
+                <img src={data.url} className="img-fluid" alt="..." />
+                <p className="card-text">{data.date}</p>
             </div>
-            <div className="card-footer text-muted">
-                Explicación
+            <div className="card-footer text-muted lh-sm">
+                {data.explanation}
             </div>
         </div>
     )
